4bb1dbc5c29ff157fc3b578410cecf791cf45eca
newdynamicAshish <ashish.m@newdynamicstech.com>
2024-07-17 16:05:06 +0530

    Xcode  textbox added

--- SwiftUIViewDemo/ContentView.swift
+++ SwiftUIViewDemo/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Projects Updated!")
+            Text("Hello")
         }
         .padding()
     }
