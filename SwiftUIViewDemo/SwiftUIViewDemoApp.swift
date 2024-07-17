f540ba3a15a29e9a4f60958b93a03eb9e61f079d
newdynamicAshish <ashish.m@newdynamicstech.com>
2024-07-17 15:47:13 +0530

    Initial Commit

---
+++ SwiftUIViewDemo/SwiftUIViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIViewDemoApp.swift
+//  SwiftUIViewDemo
+//
+//  Created by apple on 17/07/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
